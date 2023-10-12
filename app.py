46f7605163820f67bc1e6e42cedd0498a5070b5d
KishanAdhikari <adhikarykishan11@gmail.com>
2023-10-13 02:12:10 +0545

    jwt done

--- app.py
+++ app.py
@@ -1,73 +1,36 @@
-from flask import Flask,jsonify,request
+from flask import Flask,request
 from flask_restful import Resource,Api
-
+from flask_jwt import JWT,jwt_required
+from security import authenticate,identity
 
 
 app=Flask(__name__)
-
-stores=[
-    {
-        'name':'My wonderful store',
-        'items': [
-            {
-                'name': 'My Item',
-                'Price': 15.99
-            }
-        ]
-    }
-]
-@app.route('/')
-def home():
-    return '<p> Hello, World </p>'
-
-@app.route('/store ',methods=['POST'])
-def create_store():
-    request_data=request.get_json()
-    new_store={
-        'name':request_data['name'],
-        'items' : []
-    }
-    stores.append(new_store)
-    return jsonify(new_store)
- 
-@app.route('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store['name']==name:
-            return jsonify(store)
-        else:
-            return jsonify({"message":"Not Found"})
+app.secret_key='jose'
 
 
+api=Api(app)
 
-@app.route('/stores/')
-def  get_stores():
-    return jsonify({'stores':stores})
+jwt=JWT(app,authenticate,identity)
 
-@app.route('/store/<string:name>/item',method=['POST'])
-def create_item_in_store(name):
-    request_data=request.get_json()
-    for store in stores:
-        if store['name']==name:
-            new_item={
-                'name':request_data['name'],
-                'price': request_data['price']
-
-            }
-            store['items'].append(new_item)
-            return jsonify(new_item)
-    return jsonify({'message':'store not found'})
-
-                   
-@app.route('/store/<string:name>/item')
-def get_item_in_store(name):
-    for store in stores:
-        if store['items']==name:
-            return jsonify(store['items'])
-        else:
-            return jsonify({'message':'item not found'})
+items=[]
+class Item(Resource): 
+     @jwt_required()
+     def get(self,name):
+        item=next(filter(lambda x: x['name']==name,items),None)
+        return  {'item': item},200 if item  else 404
+     def post(self,name):
+          if next(filter(lambda x:x['name']==name,items),None):
+              return {'message':"An iten with name '{}' already exists.".format(name)},400
+          data=request.get_json()
+          item={'name':name,'Price':data['price']}
+          items.append(item)
+          return  item,201
+     
+class ItemList(Resource):
+    def get(self):
+        return {'items':items}
+api.add_resource(Item,'/items/<string:name>')
+api.add_resource(ItemList,'/items')
 
 
-
-
-app.run(port=4000)
+app.run(port=4000,debug=True)
