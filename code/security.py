318f6610371ffd2121dbd3831f3747c0a0ade6ef
KishanAdhikari <adhikarykishan11@gmail.com>
2023-10-17 00:34:08 +0545

    crud with sqlite done

--- code/security.py
+++ code/security.py
@@ -1,6 +1,6 @@
 from user import User
 def authenticate(username,password):
-    user=User.find_by_username(username,None)
+    user=User.find_by_username(username)
     if user and user.password==password:
         return user
 def identity(payload):
--- code/user.py
+++ code/user.py
@@ -1,11 +1,13 @@
 import sqlite3
+from flask_restful import Resource,reqparse
 
 class User:
     def __init__(self,_id,username,password):
         self.id=_id
         self.username=username
         self.password=password
-    def find_by_username(self,username):
+    @classmethod
+    def find_by_username(cls,username):
         connection=sqlite3.connect('data.db')
         cursor=connection.cursor()
 
@@ -33,3 +35,29 @@ class User:
             user=None
         connection.close()
         return user
+    
+class UserRegister(Resource):
+    parser=reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help='This field cannot be left blank.'
+                            )
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help='This field cannot be left blank.'
+                            )
+    def post(self):
+       
+        data=UserRegister.parser.parse_args()
+      
+        if User.find_by_username(data['username']):
+            return {'message':f'A user with {data["username"]} already exists.'}
+        connection=sqlite3.connect('data.db')
+        cursor=connection.cursor()
+        query="INSERT INTO users VALUES (NULL,?,?)"
+        cursor.execute(query,(data['username'],data['password']))
+        connection.commit()
+        connection.close()
+        return {'message': 'User created Successfully'},201
