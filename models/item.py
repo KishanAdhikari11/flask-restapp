0b9a95f60527926d8abd895b0ecf447f8e688a5c
KishanAdhikari <adhikarykishan11@gmail.com>
2023-11-18 07:33:46 +0545

    DB integrated with SQL Alchemy

--- models/item.py
+++ models/item.py
@@ -1,5 +1,12 @@
 import sqlite3
-class ItemModel:
+from resources.db import db
+
+class ItemModel(db.Model):
+    __tablename__='items'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(80))
+    price=db.Column(db.Float(precision=2))
+    
     def __init__(self,name,price) :
         self.name=name
         self.price=price 
@@ -7,29 +14,18 @@ class ItemModel:
         return {'name':self.name,'price':self.price}
     @classmethod
     def find_by_name(cls,name):
-       connection=sqlite3.connect('data.db')
-       cursor=connection.cursor()
-       query="SELECT * FROM items WHERE name=?"
-       result=cursor.execute(query,(name,))
-       row=result.fetchone()
-       connection.close()
-       if row:
-           return cls(*row)
-    @classmethod
-    def insert(cls,item):
-        connection=sqlite3.connect('data.db')
-        cursor=connection.cursor()
-        query="INSERT INTO items VALUES (?,?)"
-        cursor.execute(query,(item['name'],item['price']))
-        connection.commit()
-        connection.close()
-    @classmethod
-    def update(cls,item):
-        connection=sqlite3.connect('data.db')
-        cursor=connection.cursor()
-        query="UPDATE items SET price=? WHERE name=?"
-        cursor.execute(query,(item['price'],item['name']))
+       return cls.query.filter_by(name=name).first()
+    
 
-        return {'Items':item}
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+       
+   
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+        
     
     
