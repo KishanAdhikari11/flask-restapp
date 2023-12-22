84769c4b16940e1c611cbbaa1a24fc6af89d77c5
Kishan <adhikarykishan11@gmail.com>
2023-12-22 15:35:52 +0545

    sql alchemy used

--- security.py
+++ security.py
@@ -1,10 +1,9 @@
 from models.user import UserModel
-from flask_jwt_extended import create_access_token
+from werkzeug.security import safe_str_cmp
 def authenticate(username,password):
     user=UserModel.find_by_username(username)
-    if user and user.password==password:
-        access_token=create_access_token(identity=user.id)
-        return access_token
+    if user and safe_str_cmp(user.password,password):
+        return user
 
 def identity(payload):
     user_id=payload['identity']
